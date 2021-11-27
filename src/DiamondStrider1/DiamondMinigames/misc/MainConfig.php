11173a24b93089f0080a31014b262ed1c30d0660
DiamondStrider1 <62265561+Swift-Strider@users.noreply.github.com>
2021-11-27 10:55:45 -0700

    add message config for more customization

--- src/DiamondStrider1/DiamondMinigames/misc/MainConfig.php
+++ src/DiamondStrider1/DiamondMinigames/misc/MainConfig.php
@@ -5,16 +5,22 @@ declare(strict_types=1);
 namespace DiamondStrider1\DiamondMinigames\misc;
 
 use DiamondStrider1\DiamondMinigames\data\metadata\IDefaultProvider;
+use DiamondStrider1\DiamondMinigames\data\metadata\ObjectType;
 
 class MainConfig implements IDefaultProvider
 {
   /** @phpstan-var array<string, mixed> */
   private static array $defaults;
+
+  #[ObjectType(MessageConfig::class, "player-joined", "Sent to players in queue when another player joins")]
+  public MessageConfig $playerJoined;
   
   public static function getDefaults(): array
   {
     if (!isset(self::$defaults)) {
-      self::$defaults = [];
+      self::$defaults = [
+        "player-joined" => new MessageConfig('[JOIN] $player [$min/$max]', "chat"),
+      ];
     }
 
     return self::$defaults;
---
+++ src/DiamondStrider1/DiamondMinigames/misc/MessageConfig.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DiamondStrider1\DiamondMinigames\misc;
+
+use DiamondStrider1\DiamondMinigames\data\metadata\IValidationProvider;
+use DiamondStrider1\DiamondMinigames\data\metadata\StringType;
+use pocketmine\Player;
+
+class MessageConfig implements IValidationProvider
+{
+  public function __construct(
+    // TODO: Have description point people to README.md for possible $-arguments
+    #[StringType("message", '$arg will be replaced with it\'s value')]
+    public string $message = "",
+    #[StringType("display", "May be: (chat, actionbar)")]
+    public string $display = "<unset>"
+  ) {
+  }
+
+  public function isValid(): Result
+  {
+    switch ($this->display) {
+      case "chat":
+      case "actionbar":
+        return Result::ok();
+      default:
+        return Result::error(
+          "The key `display` must be chat or actionbar.\n" . "{$this->display} is not valid"
+        );
+    }
+  }
+
+  /**
+   * @phpstan-param array<string, string> $args with leading `$`
+   * @param Player[] $players
+   */
+  public function sendMessage(array $args, array $players): void
+  {
+    $message = str_replace(array_keys($args), array_values($args), $this->message);
+
+    switch ($this->display) {
+      case "chat":
+        foreach ($players as $player) {
+          $player->sendMessage($message);
+        }
+        break;
+      case "actionbar";
+        foreach ($players as $player) {
+          $player->sendActionBarMessage($message);
+        }
+        break;
+    }
+  }
+}
