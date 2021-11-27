4dc8ef5dbd03a88e667283bd2858468931896c99
DiamondStrider1 <62265561+Swift-Strider@users.noreply.github.com>
2021-11-27 10:55:05 -0700

    add more phpdocs to IDefualtProvider

--- src/DiamondStrider1/DiamondMinigames/data/metadata/IDefaultProvider.php
+++ src/DiamondStrider1/DiamondMinigames/data/metadata/IDefaultProvider.php
@@ -4,8 +4,24 @@ declare(strict_types=1);
 
 namespace DiamondStrider1\DiamondMinigames\data\metadata;
 
+/**
+ * Provides a defaults array where config-key => value
+ */
 interface IDefaultProvider
 {
-  /** @phpstan-return array<string, mixed> */
+  /**
+   * @phpstan-return array<string, mixed>
+   * config-key => value
+   * 
+   * The values this function returns must be
+   * fully parsed.
+   * 
+   * Ex: The defaults in yaml
+   * ```yaml
+   * position: [0, 2, 4]
+   * ```
+   * would lead to `getDefaults()` returning
+   * `["position" => new Vector3(0, 2, 4)]`
+   */
   public static function getDefaults(): array;
 }
