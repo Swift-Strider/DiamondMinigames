3ba1b67f30f663570d8e2d671f5905f2922a7649
DiamondStrider1 <62265561+Swift-Strider@users.noreply.github.com>
2021-11-30 22:15:12 -0600

    make LastTeamWinImpl implement Listener directly

--- src/DiamondStrider1/DiamondMinigames/minigame/impl/LastTeamWinImpl.php
+++ src/DiamondStrider1/DiamondMinigames/minigame/impl/LastTeamWinImpl.php
@@ -9,32 +9,29 @@ use DiamondStrider1\DiamondMinigames\Plugin;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
 
-class LastTeamWinImpl implements IStrategyImpl
+class LastTeamWinImpl implements IStrategyImpl, Listener
 {
+  private Minigame $minigame;
   public function onInit(Minigame $minigame): void
   {
+    $this->minigame = $minigame;
     $plugin = Plugin::getInstance();
-    $plugin->getServer()->getPluginManager()->registerEvents(new class($minigame) implements Listener
-    {
-      public function __construct(
-        private Minigame $minigame
-      ) {
-      }
-
-      public function onPlayerDeath(PlayerDeathEvent $ev): void
-      {
-        $died = $ev->getPlayer();
-        if (!$this->minigame->hasPlayer($died)) return;
-
-        $this->minigame->removePlayer($died);
-        if (count($aliveTeams = $this->minigame->getPlayingTeams()) === 1) {
-          $this->minigame->endGame($aliveTeams[0]);
-        }
-      }
-    }, $plugin);
+    $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
   }
 
   public function onDestroy(): void
   {
+  }
+
+  public function onPlayerDeath(PlayerDeathEvent $ev): void
+  {
+    if ($this->minigame->getState() !== Minigame::RUNNING) return;
+    $died = $ev->getPlayer();
+    if (!$this->minigame->hasPlayer($died)) return;
+
+    $this->minigame->removePlayer($died);
+    if (count($aliveTeams = $this->minigame->getPlayingTeams()) === 1) {
+      $this->minigame->endGame($aliveTeams[0]);
+    }
   }
 }
