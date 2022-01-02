61453fd60a113fc509e5cde86137ce2b4ab43925
DiamondStrider1 <62265561+Swift-Strider@users.noreply.github.com>
2022-01-02 13:53:04 -0600

    show info to console users for /diamondminigames

--- src/DiamondStrider1/DiamondMinigames/commands/MainCommand.php
+++ src/DiamondStrider1/DiamondMinigames/commands/MainCommand.php
@@ -14,13 +14,20 @@ class MainCommand extends BaseCommand
   public function prepare(): void
   {
     $this->setPermission("diamondminigames.manage");
-    $this->onlyPlayers = true;
   }
 
   public function onRun(CommandSender $sender, string $commandLabel, array $args): void
   {
-    if (!($sender instanceof Player))
-      throw new AssertionError('$sender is guaranteed to be a player');
+    if (!($sender instanceof Player)) {
+      $mgs = $this->getOwningPlugin()->getMinigameStore()->getAll();
+      $wts = $this->getOwningPlugin()->getWorldTemplateManager()->getAll();
+      $sender->sendMessage(sprintf("Minigames (%d):\t%s", count($mgs), implode(", ", array_map(fn ($mg) => "\"$mg->name\"", $mgs))));
+      $sender->sendMessage(sprintf("Minigames (%d):\t%s", count($wts), implode(", ", array_map(fn ($wt) => "\"{$wt->getName()}\"", $wts))));
+      $sender->sendMessage("--- HELP:");
+      $sender->sendMessage("You may make edits to the config files,");
+      $sender->sendMessage("Or interactively edit them by running `/diamondminigames` as a player.");
+      return;
+    }
     (new ManageForm)->sendTo($sender);
   }
 }
